ccbb730bf0c393b59e6f838eb57cb70309221189
sabridemirr <sabridemir451@gmail.com>
2026-08-10 16:53:30 +0300

    Added getters & setters methods

--- src/Teacher.java
+++ src/Teacher.java
@@ -1,9 +1,9 @@
 class Teacher {
 
-String name;
-int age;
-int teacherId;
-String subject;
+private String name;
+private int age;
+private int teacherId;
+private String subject;
 
 Teacher(String name, int age, int teacherId, String subject){
     this.name = name;
@@ -12,7 +12,39 @@ Teacher(String name, int age, int teacherId, String subject){
     this.subject = subject;
 }
 
-void displayTeacher(){
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setAge(int age) {
+        this.age = age;
+    }
+
+    public int getAge() {
+        return age;
+    }
+
+    public void setTeacherId(int teacherId) {
+        this.teacherId = teacherId;
+    }
+
+    public int getTeacherId() {
+        return teacherId;
+    }
+
+    public void setSubject(String subject) {
+        this.subject = subject;
+    }
+
+    public String getSubject() {
+        return subject;
+    }
+
+    void displayTeacher(){
     System.out.println("Name: " + name);
     System.out.println("Age: " + age);
     System.out.println("Teacher ID: " + teacherId);
